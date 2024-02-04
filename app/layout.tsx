7a16919e2962cdd57616db5bba5b87ea8cba237a
m1sk9 <me@m1sk9.dev>
2024-02-05 03:21:30 +0900

    feat: setup metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,44 @@
 import clsx from 'clsx';
+import { Metadata } from 'next';
+import { Twitter } from 'next/dist/lib/metadata/types/twitter-types';
 import { Noto_Sans_JP } from 'next/font/google';
 
+import pkg from '../package.json' assert { type: 'json' };
 import './globals.css';
 
 const notoSansJP = Noto_Sans_JP({
   subsets: ['latin'],
   variable: '--font-noto-sans-jp'
 });
+
+const twitterMetadata: Twitter = {
+  creator: '@m1s2r8',
+  title: pkg.name,
+  card: 'summary'
+};
+
+export const metadata: Metadata = {
+  title: {
+    default: pkg.name,
+    template: `%s | ${pkg.name}`
+  },
+  openGraph: {
+    title: pkg.name,
+    description: "m1sk9's portfolio",
+    url: 'https://m1sk9.dev',
+    type: 'website',
+    locale: 'ja_JP'
+  },
+  twitter: twitterMetadata,
+  description: "m1sk9's portfolio",
+  authors: {
+    name: 'm1sk9',
+    url: 'https://m1sk9.dev'
+  },
+  generator: 'Next.js',
+  keywords: ['m1sk9', 'portfolio'],
+  publisher: 'Vercel'
+};
 
 export default function RootLayout({
   children

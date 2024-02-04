06fc76984ef351b7bfe21472d00e29a6cf3ea363
m1sk9 <me@m1sk9.dev>
2024-02-05 03:17:57 +0900

    feat: setup favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,32 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ja" className={clsx(notoSansJP.variable, 'font-sans')}>
+      <head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicons/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicons/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicons/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/favicons/safari-pinned-tab.svg"
+          color="#000000"
+        />
+        <link rel="shortcut icon" href="/favicons/favicon.ico" />
+      </head>
       <body className=" bg-gray-900">{children}</body>
     </html>
   );

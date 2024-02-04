611f5933a0308536f92de695976e9aaacbac795f
m1sk9 <me@m1sk9.dev>
2024-02-04 21:56:47 +0900

    feat: start using TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+const tailwindcssConfig = {
+  content: ['./app/**/*.{ts,tsx}'],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
+
+export default tailwindcssConfig;

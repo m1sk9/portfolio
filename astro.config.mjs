d8f21ba713385176fde50c66c9aac05fadde8eec
m1sk9 <me@m1sk9.dev>
2024-05-20 03:24:41 +0900

    feat: setup panda css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import vercel from '@astrojs/vercel/serverless';
+import pandacss from '@pandacss/astro';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel()
+  adapter: vercel(),
+  integrations: [pandacss()]
 });

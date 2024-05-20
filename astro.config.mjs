6295fc96b2c7d653b549c9eb6366a22f00c33a61
m1sk9 <me@m1sk9.dev>
2024-05-21 01:47:59 +0900

    chore: disable ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
-import vercel from '@astrojs/vercel/serverless';
 import pandacss from '@pandacss/astro';
 import icon from 'astro-icon';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: vercel(),
   integrations: [
     pandacss(),
     icon({

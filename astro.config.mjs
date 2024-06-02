5e9fdca4084e079e4e6bc8a4baf6ac47467b2929
m1sk9 <me@m1sk9.dev>
2024-06-03 00:58:20 +0900

    deps: add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,17 @@
+import vercel from '@astrojs/vercel/serverless';
 import pandacss from '@pandacss/astro';
 import icon from 'astro-icon';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://m1sk9.dev',
+  output: 'hybrid',
+  adapter: vercel({
+    isr: {
+      exclude: ['/']
+    }
+  }),
   // ブログレイアウトの再デザイン時に Content Collections を使用したため, ルーティングが変更されている. そのため, リダイレクトを設定する.
   redirects: {
     '/blog/posts/2024/about-pulsate-2024-03': '/blog/about-pulsate-2024-03',

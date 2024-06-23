d95c58ea75f19178969091c63df6703c17810153
m1sk9 <me@m1sk9.dev>
2024-06-24 02:22:56 +0900

    refactor: remove `useEffect` and `useState`

--- app/components/organisms/Skill.tsx
+++ app/components/organisms/Skill.tsx
@@ -1,30 +1,37 @@
-import { useEffect, useState } from "react";
 import styles from "../../styles/components/skill.module.css";
 
 type SkillSection = "Languages" | "Frontend" | "Backend" | "Others";
 
 interface SkillsListProps {
-  sections: SkillSection[];
+  sections: SkillSection;
   skills: string[];
 }
 
+const skillsList: SkillsListProps[] = [
+  {
+    sections: "Languages",
+    skills: ["TypeScript (JavaScript)", "Kotlin", "Rust", "Python"],
+  },
+  {
+    sections: "Frontend",
+    skills: ["HTML5", "CSS", "React", "Next.js"],
+  },
+  {
+    sections: "Backend",
+    skills: ["Node.js", "Deno"],
+  },
+  {
+    sections: "Others",
+    skills: ["Git", "GitHub", "Docker"],
+  },
+];
+
 export default function Skills() {
-  const [skills, setSkills] = useState<SkillsListProps[]>([]);
-
-  useEffect(() => {
-    const fetchSkills = async () => {
-      const res = await fetch("./skills.json");
-      const data = (await res.json()) as SkillsListProps[];
-      setSkills(data);
-    };
-    fetchSkills();
-  });
-
   return (
     <div>
       <h2>Skills</h2>
       <ul className={styles.skill_list}>
-        {skills.map((i) => (
+        {skillsList.map((i) => (
           // biome-ignore lint/correctness/useJsxKeyInIterable: JSX key prop is not required for this use case
           <li>
             <p>{i.sections}</p>
